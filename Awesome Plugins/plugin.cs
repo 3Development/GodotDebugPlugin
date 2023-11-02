249a3c8a2460a0059e2be68dde7851a9255a5855
leno <leno.application@gmail.com>
2023-11-02 20:33:35 +0100

    Add new fix

--- Awesome Plugins/plugin.cs
+++ Awesome Plugins/plugin.cs
@@ -11,7 +11,8 @@ public partial class plugin : EditorPlugin
 	public override void _EnterTree()
 	{
 		// Initialization of the plugin goes here.
-		interfaceContainer =  (Control)GD.Load<PackedScene>("res://Awesome Plugins/Scene/interface.tscn").Instantiate<Control>();
+		interfaceContainer =  (Control)GD.Load<PackedScene>("./Scene/interface.tscn").Instantiate<Control>();
+		
 		AddControlToBottomPanel(interfaceContainer,"Debug tool");
 	}
 
