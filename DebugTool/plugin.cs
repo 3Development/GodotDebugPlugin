2cc3200542fe947aa02f54f6723356efc679fa50
leno <leno.application@gmail.com>
2023-11-04 18:10:39 +0100

    Refactored position of file to check if it working for another project

---
+++ DebugTool/plugin.cs
@@ -0,0 +1,25 @@
+#if TOOLS
+using Godot;
+using System;
+
+[Tool]
+public partial class plugin : EditorPlugin
+{
+	private Control interfaceContainer;
+
+	
+	public override void _EnterTree()
+	{
+		// Initialization of the plugin goes here.
+		interfaceContainer =  (Control)GD.Load<PackedScene>("./Scene/interface.tscn").Instantiate<Control>();
+		
+		AddControlToBottomPanel(interfaceContainer,"Debug tool");
+	}
+
+	public override void _ExitTree()
+	{
+		RemoveControlFromBottomPanel(interfaceContainer);
+		// Clean-up of the plugin goes here.
+	}
+}
+#endif
